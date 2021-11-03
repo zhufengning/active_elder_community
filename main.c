9c3a4f35f618d6c300ca03088a5f0634817d5be8
zhufengning <z@zhufn.fun>
2021-11-03 17:21:16 +0800

    会员列表的基本操作

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "people.h"
 int main() {
-    printf("Hello, World!\n");
+    PeopleList l = people_list_new();
+    people_list_push(&l, "haha");
+    printf("%d:%s\n", people_list_at(l, 0).id, people_list_at(l, 0).name);
     return 0;
 }
--- people.h
+++ people.h
@@ -2,9 +2,10 @@
 // Created by zfn on 2021/11/3.
 //
 
-#ifndef OLD_COMM_PEOPLE_H
-#define OLD_COMM_PEOPLE_H
-
+#ifndef ELDER_COMM_PEOPLE_H
+#define ELDER_COMM_PEOPLE_H
+#include <stdlib.h>
+#include <string.h>
 typedef struct People
 {
     int id;
@@ -17,4 +18,36 @@ typedef struct PeopleList
     int size;
 } PeopleList;
 
-#endif //OLD_COMM_PEOPLE_H
+///新建一个社区会员列表
+PeopleList people_list_new(void)
+{
+    PeopleList t;
+    t.head = NULL;
+    t.size = 0;
+    return t;
+}
+
+///往一个会员列表中插入一个新会员并分配id
+void people_list_push(PeopleList *p, char *name)
+{
+    if (p->size == 0)
+    {
+        p->head = malloc(sizeof(People));
+        ++p->size;
+    } else
+    {
+        ++p->size;
+        p->head = realloc(p->head, sizeof(People) * p->size);
+    }
+    p->head[p->size - 1].id = p->size;
+    memcpy(p->head[p->size - 1].name, name, strlen(name) * sizeof(char));
+}
+
+///获取列表中第in个会员（从零开始数）
+People people_list_at(PeopleList p, int in)
+{
+    People r = {-1, "fuck!"};
+    if (in >= p.size) return r;
+    else return p.head[in];
+}
+#endif //ELDER_COMM_PEOPLE_H
