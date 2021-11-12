aa204a5a68f9ee88d361fcf1e0266992a4cad904
愽龙 黄 <2134693323@qq.com>
2021-11-12 21:46:43 +0800

    界面一点

--- main.c
+++ main.c
@@ -5,9 +5,32 @@
 #include "facility.h"
 #include "servant.h"
 #include "house.h"
-int main() {
-    PeopleList l = people_list_new();
-    people_list_push(&l, "haha");
-    printf("%d:%s\n", people_list_at(l, 0).id, people_list_at(l, 0).name);
-    return 0;
+int main() 
+{
+	int n;
+
+	printf(
+		"亲，请输入数字\n"
+		"\t输入1选择会员管理\n"
+		"\t输入2选择房屋管理\n"
+		"\t输入3选择入住管理\n"
+		"\t输入4选择场馆设施管理\n"
+		"\t输入5选择班车路线管理\n"
+		"\t输入6选择问题反映：");
+	scanf("%d", &n);
+
+	if (n == 1)
+	{
+		printf(
+			"您下一步想要干什么？\n"
+			"\t输入1选择新建会员\n"
+            "\t输入2选择查询会员\n"
+			"\t输入3选择会员的删除与修改\n");
+	}
+
+
+	PeopleList l = people_list_new();
+	people_list_push(&l, "haha");
+	printf("%d:%s\n", people_list_at(l, 0).id, people_list_at(l, 0).name);
+	return 0;
 }
