ca96bb6d99df1a470b60027cebf46b1bff126dc3
敌 无 <2486148544@qq.com>
2021-12-03 19:54:55 +0800

    数字改东西

--- main.c
+++ main.c
@@ -31,7 +31,7 @@ int main()
         long the_n;
         if (!fst_time)
         {
-            printf("输入1继续使用本系统，输入其他数字保存并退出\n");
+            printf("输入1继续使用本系统，输入其他东西保存并退出\n");
             fflush(stdout);
             long cont;
             scanf("%104s", INPUT_DATA);
